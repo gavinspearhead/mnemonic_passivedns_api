cb59492cbe31f9484224a4561d329e7c42709c58
gavin spearhead <gavin.spearhead+github@gmail.com>
2020-06-06 00:46:33 +0200

    Added arguments parser Added api key option Added more data fields

--- mnemonic request.py
+++ mnemonic request.py
@@ -1,8 +1,10 @@
-import sys
+import argparse
+import logging
 from datetime import datetime
-from pprint import pprint
 
 import requests
+
+logging.getLogger().setLevel(logging.INFO)
 
 
 class MnemonicEntry:
@@ -11,10 +13,12 @@ class MnemonicEntry:
         self.data = data
 
     def __repr__(self):
-        return self.data
+        return " ".join([self.query, self.answer, self.rrtype, str(self.firstseen), str(self.lastseen), self.rrclass,
+                         str(self.times), str(self.ttl)])
 
     def __str__(self):
-        return " ".join([self.query, self.answer, self.rrtype])
+        return ", ".join(
+            [self.query, self.answer, self.rrtype, str(self.firstseen), str(self.lastseen), str(self.times)])
 
     def __getitem__(self, item):
         return self.data[item]
@@ -43,11 +47,11 @@ class MnemonicEntry:
 
     @property
     def rrtype(self):
-        return self['rrtype']
+        return self['rrtype'].upper()
 
     @property
     def rrclass(self):
-        return self['rrclass']
+        return self['rrclass'].upper()
 
     @property
     def answer(self):
@@ -64,31 +68,63 @@ class Mnemonic:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
     }
 
-    def __call__(self, search_term, limit=100):
+    def __init__(self, api_key=None):
+
+        if api_key:
+            self.headers['Argus-API-Key'] = api_key
+
+    def __call__(self, search_term, limit=100, offset=0, rrtype=None, rrclass=None):
         data = {
             "query": search_term,
             "aggregateResult": True,
             "includeAnonymousResults": True,
-            "rrClass": [],
-            "rrType": [],
+            "rrClass": [] if rrclass is None else [r.lower() for r in rrclass],
+            "rrType": [] if rrtype is None else [r.lower() for r in rrtype],
             "customerID": [],
             "tlp": [],
-            "offset": 0,
-            "limit": limit
+            "offset": int(offset),
+            "limit": int(limit)
         }
         res = requests.post(self.url, headers=self.headers, json=data)
+        json_val = res.json()
+        logging.info("{}+{}/{} Elements retrieved".format(json_val['offset'], json_val['size'], json_val['count']))
         if res.status_code == 200:
-            return map(MnemonicEntry, res.json()['data'])
+            return list(map(MnemonicEntry, json_val['data']))
         else:
-            return None
+            logging.error(json_val['messages'][0]['message'])
+            raise ValueError(json_val['messages'][0]['message'])
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        exit()
-    search_term = sys.argv[1]
+    parser = argparse.ArgumentParser("Access to Mnemonic Passive DNS API")
+    parser.add_argument('-l', '--limit', metavar='L', type=int, help="limit the number of results", default=100)
+    parser.add_argument('-o', '--offset', metavar='O', type=int, help="offset the number of results", default=0)
+    parser.add_argument('name', metavar='N', type=str, help="name to lookup")
+    parser.add_argument('-a', '--all', help="Iterate through all results", action='store_true')
+    parser.add_argument('-r', '--rrtype', help="RR types to look up", default=[], nargs='+')
+    parser.add_argument('-c', '--rrclass', help="RR classes to look up", default=[], nargs='+')
+    args = parser.parse_args()
+
+    search_term = args.name
 
     m = Mnemonic()
-    res = m(search_term, 1000)
-    for x in res:
-        print(x.lastseen, x.firstseen, x.answer, x.query, x.rrtype)
+    results = []
+    try:
+        if args.all:
+            if args.limit <= 0:
+                args.limit = 100
+            offset = 0
+            while True:
+                res = m(search_term, args.limit, offset)
+                if not res:
+                    break
+                results += res
+                offset += args.limit
+        else:
+            results = m(search_term, args.limit, args.offset, args.rrtype, args.rrclass)
+
+        for x in results:
+            print(x)
+    except ValueError as e:
+        print(e)
+        pass
